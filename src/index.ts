d8856217cd42e1dd9034724e67223c182e53946e
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-13 15:06:11 +0800

    feat(logger): Start serving

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,38 @@
+import path from 'path'
+import dotenv from 'dotenv'
+
+import app from './app'
 import { logger } from './helpers/logger'
 
-logger.info('Info from index')
-logger.error('Error from index')
+dotenv.config({ path: path.join(__dirname, '../.env') })
+const PORT = process.env.PORT
+
+const server = app.listen(PORT, () => {
+  logger.info(`Application is up and running on port ${PORT}`)
+})
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed')
+      process.exit(1)
+    })
+  } else {
+    process.exit(1)
+  }
+}
+
+const unexpectedErrorHandler = (error: Error) => {
+  logger.error(error)
+  exitHandler()
+}
+
+process.on('uncaughtException', unexpectedErrorHandler)
+process.on('unhandledRejection', unexpectedErrorHandler)
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received')
+  if (server) {
+    server.close()
+  }
+})
