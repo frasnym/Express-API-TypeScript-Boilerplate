5de707938e970201f5644ced8041c04b44aa5ceb
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-14 10:41:06 +0800

    feat(env): Add centralized load and Joi validation

---
+++ src/helpers/envVars.ts
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv'
+import path from 'path'
+import Joi from 'joi'
+
+type EnvVars = {
+  NODE_ENV: string
+  PORT: number
+}
+
+dotenv.config({ path: path.join(__dirname, '../../.env') })
+
+const envVarsSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+  PORT: Joi.number().default(3000)
+}).unknown()
+
+const { value, error } = envVarsSchema.validate(process.env)
+
+if (error) {
+  throw new Error(`Environment validation error: ${error.message}`)
+}
+
+const envVars: EnvVars = value
+
+export default {
+  env: envVars.NODE_ENV,
+  port: envVars.PORT
+}
