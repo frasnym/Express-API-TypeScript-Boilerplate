25ccb4e1625d83a42fc3d54acdbca4b0612b0f3d
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-15 13:58:35 +0800

    feat(error): Apply JSend formatted response

--- src/routes/v1/auth-router.ts
+++ src/routes/v1/auth-router.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { validate } from '../../middlewares/validate'
+import { SuccessResponse } from '../../utils/jsend/success'
 import { signupSchema } from '../../validations'
 
 const router = Router()
 
 router.post('/signup', validate(signupSchema), (req, res) => {
-  res.status(201).send(req.body)
+  res.status(201).send(new SuccessResponse(req.body).serializeResponse())
 })
 
 export { router as authRouter }
