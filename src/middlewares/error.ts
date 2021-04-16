ecc7ce79459e334e1a9776ebce4a03eb231db797
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-16 12:45:57 +0800

    feat(error): Update function to catch variable from jsend

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,20 +1,37 @@
 import { ErrorRequestHandler } from 'express'
 import envVars from '../config/envVars'
 import { logger } from '../config/logger'
+import { ErrorResponse, FailResponse } from '../utils/jsend'
 
-const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
-  const { statusCode, data, status } = err
+interface JsendError {
+  statusCode: number
+  status: string
+  message?: string
+  data?: any
+  code?: number
+  stack?: any
+}
+
+const errorHandler: ErrorRequestHandler = (
+  err: JsendError,
+  _req,
+  res,
+  _next
+) => {
+  const { statusCode, status } = err
 
   res.locals.errorMessage = err.message
 
   const response = {
     status,
-    data,
+    message: err.message,
+    ...(err instanceof FailResponse && { data: err.data }),
+    ...(err instanceof ErrorResponse && { code: err.code }),
     ...(envVars.env === 'development' && { stack: err.stack })
   }
 
   if (envVars.env === 'development') {
-    logger.error(err)
+    logger.error(JSON.stringify(response))
   }
 
   return res.status(statusCode).send(response)
