cdb61436f824a96acafd4f044359adcc6291f571
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-16 15:27:11 +0800

    test(error): Ensure Errorhandler should send proper JSend Error response

--- src/middlewares/__test__/error.spec.ts
+++ src/middlewares/__test__/error.spec.ts
@@ -1,7 +1,7 @@
 import httpMocks from 'node-mocks-http'
 import envVars from '../../config/envVars'
 import { logger } from '../../config/logger'
-import { FailResponse } from '../../utils/jsend'
+import { ErrorResponse, FailResponse } from '../../utils/jsend'
 import { errorHandler } from '../error'
 
 describe('Error Middleware', () => {
@@ -11,7 +11,7 @@ describe('Error Middleware', () => {
       jest.spyOn(logger, 'error').mockImplementation(() => {})
     })
 
-    test('should send proper error response and put the error message in res.locals', () => {
+    test('should send proper JSend Fail response and put the error message in res.locals', () => {
       const error = new FailResponse(400, 'Any message', 'Any data')
       const res = httpMocks.createResponse()
       const sendSpy = jest.spyOn(res, 'send')
@@ -28,7 +28,7 @@ describe('Error Middleware', () => {
       expect(res.locals.errorMessage).toBe(error.message)
     })
 
-    test('should put the error stack in the response if in development mode', () => {
+    test('should put the stack in the JSend Fail response if in development mode', () => {
       envVars.env = 'development'
       const error = new FailResponse(400, 'Any message', 'Any data')
       const res = httpMocks.createResponse()
@@ -39,6 +39,39 @@ describe('Error Middleware', () => {
       expect(sendSpy).toHaveBeenCalledWith(
         expect.objectContaining({
           data: error.data,
+          message: error.message,
+          stack: error.stack
+        })
+      )
+      envVars.env = process.env.NODE_ENV!
+    })
+
+    test('should send proper JSend Error response and put the error message in res.locals', () => {
+      const error = new ErrorResponse(500, 'Any message')
+      const res = httpMocks.createResponse()
+      const sendSpy = jest.spyOn(res, 'send')
+
+      errorHandler(error, httpMocks.createRequest(), res, () => {})
+
+      expect(sendSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          message: error.message,
+          status: 'error'
+        })
+      )
+      expect(res.locals.errorMessage).toBe(error.message)
+    })
+
+    test('should put the stack in the JSend Error response if in development mode', () => {
+      envVars.env = 'development'
+      const error = new ErrorResponse(400, 'Any message')
+      const res = httpMocks.createResponse()
+      const sendSpy = jest.spyOn(res, 'send')
+
+      errorHandler(error, httpMocks.createRequest(), res, () => {})
+
+      expect(sendSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
           message: error.message,
           stack: error.stack
         })
