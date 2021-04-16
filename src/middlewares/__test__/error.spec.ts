f9bbb2e4dd8da60bccec019c3a1b157a2cc55c57
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-16 15:16:12 +0800

    test(error): Ensure ErrorHandler should put the error stack in the response if in development mode

--- src/middlewares/__test__/error.spec.ts
+++ src/middlewares/__test__/error.spec.ts
@@ -1,9 +1,16 @@
 import httpMocks from 'node-mocks-http'
+import envVars from '../../config/envVars'
+import { logger } from '../../config/logger'
 import { FailResponse } from '../../utils/jsend'
 import { errorHandler } from '../error'
 
 describe('Error Middleware', () => {
   describe('ErrorHandler', () => {
+    beforeEach(() => {
+      // @ts-ignore
+      jest.spyOn(logger, 'error').mockImplementation(() => {})
+    })
+
     test('should send proper error response and put the error message in res.locals', () => {
       const error = new FailResponse(400, 'Any message', 'Any data')
       const res = httpMocks.createResponse()
@@ -19,6 +26,24 @@ describe('Error Middleware', () => {
         })
       )
       expect(res.locals.errorMessage).toBe(error.message)
+    })
+
+    test('should put the error stack in the response if in development mode', () => {
+      envVars.env = 'development'
+      const error = new FailResponse(400, 'Any message', 'Any data')
+      const res = httpMocks.createResponse()
+      const sendSpy = jest.spyOn(res, 'send')
+
+      errorHandler(error, httpMocks.createRequest(), res, () => {})
+
+      expect(sendSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          data: error.data,
+          message: error.message,
+          stack: error.stack
+        })
+      )
+      envVars.env = process.env.NODE_ENV!
     })
   })
 })
