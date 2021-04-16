336b2ab4470b4a031d5cf4f9844c17ea81392f83
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-16 12:42:47 +0800

    feat(auth): Declare auth env variable

--- src/config/envVars.ts
+++ src/config/envVars.ts
@@ -1,17 +1,26 @@
 import dotenv from 'dotenv'
 import path from 'path'
 import Joi from 'joi'
-
-type EnvVars = {
-  NODE_ENV: string
-  PORT: number
-}
+import { EnvVars } from '../types/rest-api'
 
 dotenv.config({ path: path.join(__dirname, '../../.env') })
 
 const envVarsSchema = Joi.object({
   NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-  PORT: Joi.number().default(3000)
+  PORT: Joi.number().default(3000),
+  JWT_SECRET: Joi.string().required().description('JWT secret key'),
+  JWT_ACCESS_EXPIRATION_MINUTES: Joi.number()
+    .default(30)
+    .description('minutes after which access tokens expire'),
+  JWT_REFRESH_EXPIRATION_DAYS: Joi.number()
+    .default(30)
+    .description('days after which refresh tokens expire'),
+  JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
+    .default(10)
+    .description('minutes after which reset password token expires'),
+  JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
+    .default(10)
+    .description('minutes after which verify email token expires')
 }).unknown()
 
 const { value, error } = envVarsSchema.validate(process.env)
@@ -23,5 +32,13 @@ const envVars: EnvVars = value
 
 export default {
   env: envVars.NODE_ENV,
-  port: envVars.PORT
+  port: envVars.PORT,
+  jwt: {
+    secret: envVars.JWT_SECRET,
+    accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
+    refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
+    resetPasswordExpirationMinutes:
+      envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
+    verifyEmailExpirationMinutes: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES
+  }
 }
