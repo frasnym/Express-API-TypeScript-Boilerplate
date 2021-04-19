48bcf34295aab6389fc3b0f966eae235abfe8a88
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-19 13:04:55 +0800

    test(auth): Create token fixture

---
+++ tests/fixtures/token-fixture.ts
@@ -0,0 +1,18 @@
+import envVars from '../../src/config/envVars'
+import { generateToken } from '../../src/services/token-service'
+import { dateAdd } from '../../src/utils/date'
+const { tokenTypes } = require('../../src/config/tokens')
+
+const NOW = new Date()
+const accessTokenExpires = dateAdd(
+  NOW,
+  'minute',
+  envVars.jwt.accessExpirationMinutes
+)
+const accessToken = generateToken(
+  Math.random(),
+  accessTokenExpires,
+  tokenTypes.ACCESS
+)
+
+export { accessToken }
