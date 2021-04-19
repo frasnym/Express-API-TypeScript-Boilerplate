afbd8f5bf77433cfb774742745da6ce172babdd4
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-19 14:37:15 +0800

    docs(users): Add GET logged user data documentation

--- src/routes/v1/user-route.ts
+++ src/routes/v1/user-route.ts
@@ -7,3 +7,32 @@ const router = Router()
 router.get('/', auth(), userController.getUser)
 
 export { router as userRoute }
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User related API endpoints
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Get logged user data
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       "200":
+ *         description: Success get logged user data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   $ref: '#/components/schemas/User'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ */
