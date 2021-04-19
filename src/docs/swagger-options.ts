d10ab839bcb8d7c41f4ff971b59408b3964f2114
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-19 14:09:58 +0800

    docs: Setup swagger

---
+++ src/docs/swagger-options.ts
@@ -0,0 +1,29 @@
+import swaggerJSDoc from 'swagger-jsdoc'
+
+import { version } from '../../package.json'
+import envVars from '../config/envVars'
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'NodeJS API Server Boilerplate',
+      version,
+      description: 'NodeJS API Server Boilerplate Documentation',
+      license: {
+        name: 'MIT',
+        url:
+          'https://github.com/frasnym/Express-API-TypeScript-Boilerplate/blob/main/LICENSE'
+      }
+    },
+    servers: [
+      {
+        url: `http://localhost:${envVars.port}/v1`,
+        description: 'Local server'
+      }
+    ]
+  },
+  apis: ['./src/docs/*.yml', './src/routes/v1/*.ts']
+}
+
+export { options as swaggerJSDocOptions }
