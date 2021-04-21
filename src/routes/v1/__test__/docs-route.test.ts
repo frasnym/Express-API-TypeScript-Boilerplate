1c68c3597c49ea75884b0255e3ad297f97f819b6
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-21 11:14:36 +0800

    docs(routes): Disable error logging

--- src/routes/v1/__test__/docs-route.test.ts
+++ src/routes/v1/__test__/docs-route.test.ts
@@ -1,10 +1,17 @@
 import request from 'supertest'
+import { logger } from '../../../config/logger'
+
 process.env.NODE_ENV = 'production'
 // eslint-disable-next-line import/first
 import app from '../../../app'
 
 describe('Docs routes', () => {
   describe('GET /v1/docs', () => {
+    beforeEach(() => {
+      // @ts-ignore
+      jest.spyOn(logger, 'error').mockImplementation(() => {})
+    })
+
     test('should return 404 when running in production', async () => {
       await request(app).get('/v1/docs').expect(404)
     })
