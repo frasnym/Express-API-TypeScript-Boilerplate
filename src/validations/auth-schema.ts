ba8496adf398b0f74efaa748fd000ae293a52903
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-21 15:08:07 +0800

    feat(validate): Apply converted value from validate to request

--- src/validations/auth-schema.ts
+++ src/validations/auth-schema.ts
@@ -6,7 +6,7 @@ const signupSchema: IValidation = {
   body: Joi.object({
     name: Joi.string().required().min(3),
     phone: Joi.string().required().custom(phone),
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email().lowercase(),
     pin: Joi.string().length(6).required().custom(pin),
     password: Joi.string().min(8).required().custom(password)
   })
