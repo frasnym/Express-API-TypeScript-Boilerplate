3af9d624404082434059d412c4ef4a88caf952fc
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-21 13:30:54 +0800

    test(sequelize): Apply sqlite3:memory for testing

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,16 +2,25 @@ import * as sequelize from 'sequelize'
 import { UserFactory } from '../models/user-model'
 import envVars from './envVars'
 
-export const dbConfig = new sequelize.Sequelize(
-  envVars.postgres.database,
-  envVars.postgres.user,
-  envVars.postgres.password,
-  {
-    dialect: 'postgres',
-    host: envVars.postgres.host,
-    port: envVars.postgres.port,
+let dbConfig: sequelize.Sequelize
+if (envVars.env === 'test') {
+  dbConfig = new sequelize.Sequelize('sqlite::memory', {
     logging: false
-  }
-)
+  })
+} else {
+  dbConfig = new sequelize.Sequelize(
+    envVars.postgres.database,
+    envVars.postgres.user,
+    envVars.postgres.password,
+    {
+      dialect: 'postgres',
+      host: envVars.postgres.host,
+      port: envVars.postgres.port,
+      logging: false
+    }
+  )
+}
+
+export { dbConfig }
 
 export const User = UserFactory(dbConfig)
