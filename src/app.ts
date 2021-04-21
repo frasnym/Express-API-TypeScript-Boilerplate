f59d939a71a5f7407dde59d5c864cb4f7bc628d6
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-21 10:19:21 +0800

    feat(app): Enable reponse on invalid route

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ import * as morgan from './config/morgan'
 import { jwtStrategy } from './config/passport'
 import { errorHandler } from './middlewares/error'
 import { router } from './routes/v1'
+import { FailResponse } from './utils/jsend'
+
+import { version } from '../package.json'
 
 const app: Application = express()
 
@@ -22,9 +25,17 @@ app.use(express.json())
 app.use(passport.initialize())
 passport.use('jwt', jwtStrategy)
 
+// Available routes
 app.use('/v1', router)
-app.use('/', (_req, res) => {
-  res.send('Boilerplate version 1.0.0')
+
+// Exactly base url
+app.use(/^[/]{1}$/, (_req, res) => {
+  res.send(`Boilerplate version ${version}`)
+})
+
+// send back a 404 error for any unknown api request
+app.use(/(?<=.{1}).+/, () => {
+  throw new FailResponse(404, 'Not found', 'Not found')
 })
 
 // error handler middleware
