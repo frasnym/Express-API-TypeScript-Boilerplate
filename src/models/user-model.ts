4979a615ba7590bd5b2079c2de731d759b076068
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-21 16:57:31 +0800

    feat(auth): Add scope and beforeCreate hooks on user model

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -1,43 +1,66 @@
 import { DataTypes, Op, Sequelize } from 'sequelize'
+import bcrypt from 'bcryptjs'
 import { UserStatic } from '../types/rest-api'
 
 export function UserFactory(sequelize: Sequelize): UserStatic {
-  const User = <UserStatic>sequelize.define('users', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
+  const User = <UserStatic>sequelize.define(
+    'users',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      pin: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+      }
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    pin: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW
+    {
+      scopes: {
+        withoutCredentials: {
+          attributes: {
+            exclude: ['password', 'pin']
+          }
+        }
+      }
+    }
+  )
+
+  User.beforeCreate(async (user, _options) => {
+    if (user.changed('password')) {
+      user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    if (user.changed('pin')) {
+      user.pin = await bcrypt.hash(user.pin, 8)
     }
   })
 
