1fa499600da623237157f1ed5d844172eefd66d2
Nyoman Frastyawan <frastyawan.nym@gmail.com>
2021-04-28 12:43:59 +0800

    test(users): Ensure getCurrentUser should return expected response

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,9 +1,16 @@
+import { UserAttributes } from '../types/rest-api'
 import { catchAsync } from '../utils/catch-async'
 import { SuccessResponse } from '../utils/jsend'
 
 const getUser = catchAsync(async (req, res) => {
-  const user = req.user
-  res.status(200).send(new SuccessResponse({ user }).serializeResponse())
+  const user: Partial<UserAttributes> = req.user!
+
+  delete user.password
+  delete user.pin
+  delete user.createdAt
+  delete user.updatedAt
+
+  res.status(200).send(new SuccessResponse(user).serializeResponse())
 })
 
 export { getUser }
--- src/routes/v1/__test__/user-route.test.ts
+++ src/routes/v1/__test__/user-route.test.ts
@@ -1,18 +1,24 @@
 import request from 'supertest'
-import { accessToken } from '../../../../tests/fixtures/token-fixture'
+import { useOneAccessToken } from '../../../../tests/fixtures/token-fixture'
+import { insertUsers, userOne } from '../../../../tests/fixtures/user-fixture'
 import app from '../../../app'
 
 describe('User routes', () => {
   describe('GET /v1/users', () => {
     test('should return 200 and apply the default query options', async () => {
+      await insertUsers([userOne])
+
       const res = await request(app)
         .get('/v1/users')
-        .set('Authorization', `Bearer ${accessToken}`)
+        .set('Authorization', `Bearer ${useOneAccessToken}`)
         .send()
         .expect(200)
 
-      expect(res.body.data.user).toHaveProperty('name')
-      // TODO: Check all returned value
+      expect(res.body.data).toHaveProperty('name')
+      expect(res.body.data).toHaveProperty('phone')
+      expect(res.body.data).toHaveProperty('email')
+      expect(res.body.data).not.toHaveProperty('password')
+      expect(res.body.data).not.toHaveProperty('pin')
     })
 
     test('should return 401 if access token is missing', async () => {
